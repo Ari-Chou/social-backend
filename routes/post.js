0d1e43b05960d039ad4a38fba742b23aa20cc7ee
AriChou <46300506+Ari-Chou@users.noreply.github.com>
2021-09-06 17:59:59 +0800

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -49,7 +49,7 @@ postRouter.put("/:id/like", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(200).json("The post has been liked");
+      res.status(200).json("The post has been liked.");
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } });
       res.status(200).json("The post has been disliked");
